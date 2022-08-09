4f663e24ea31c2ec2707434e39ee8211078b75b7
Oleh Kazban <oleh.kazban@globallogic.com>
2022-08-09 12:00:22 +0300

    Update

--- create.ts
+++ create.ts
@@ -2,27 +2,27 @@ import { Observable, from, of, throwError, interval, timer } from "rxjs";
 
 // Create an Observable
 export const sampleObservable = new Observable(observer => {
-	observer.next('1 sync');
+  observer.next('1 sync');
   observer.next('2 sync');
   observer.next('3 sync');
 
   setTimeout(() => {
-    observer.next('4 async');
+    observer.next('4 async 1 sec');
   }, 1000);
   setTimeout(() => {
-    observer.next('5 async');
+    observer.next('5 async 3 sec');
   }, 3000);
 
   // observer.complete();
 });
 export const observableFrom = from('https://api.github.com/users/');
 export const observableOf = of({
-	user: {
-		firstName: 'Oleh',
-		lastName: 'Kazban'
-	},
-	authorities: ['USER', 'SYSTEM_ADMIN'],
-	uuid: '57b81599-ec38-4c6c-83f0-68a3f71bbee1'
+  user: {
+    firstName: 'Oleh',
+    lastName: 'Kazban'
+  },
+  authorities: ['USER', 'SYSTEM_ADMIN'],
+  uuid: '57b81599-ec38-4c6c-83f0-68a3f71bbee1'
 });
 export const observableArray = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 export const observableUsersArray = from([
